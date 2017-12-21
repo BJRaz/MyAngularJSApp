0af3b6eb927eff424777cbdaf6186e42b58b7a3e
Brian <brian.juul.rasmussen@gmail.com>
2017-12-21 23:07:24 +0100

    optimeret system.config.js til at indlæse moduler inden maps

--- system.config.js
+++ system.config.js
@@ -1,25 +1,36 @@
 SystemJS.config({
-    baseURL: "/",
-    paths: {
-        "app:*": "app/*",
-        "controllers:*": "app/controllers/*",
-        "directives:*": "app/directives/*"
-    },
+    baseURL: "/app",
+    // paths: {
+    //     "app:*": "*",
+    //     "controllers:*": "app/controllers/*",
+    //     "directives:*": "app/directives/*",
+    // },
     map: {
-         "controllers/my.controller": "controllers:my.controller.js",
-         "app.controller": "app:app.controller.js",
-         "directives/test": "directives:test.js",
-         "app.module": "app:app.module.js",
-        angular: "node_modules/angular/angular.js",
-        "@uirouter/angularjs": "node_modules/@uirouter/angularjs/release/angular-ui-router.js"
+        //  "controllers/my.controller": "controllers:my.controller.js",
+        //  "app.controller": "app:app.controller.js",
+        //  "directives/test": "directives:test.js",
+        //"app.module": "app:app.module.js",
+        angular: "/node_modules/angular/angular.js",
+        "@uirouter/angularjs": "/node_modules/@uirouter/angularjs/release/angular-ui-router.js"
     },
     meta: {
-        "node_modules/angular/angular.js":{
+        "/node_modules/angular/angular.js":{
             format: "global",
             exports: "angular"    
         },
-        "node_modules/@uirouter/angularjs/release/angular-ui-router.js": {           
-            deps: "node_modules/angular/angular.js"
+        "/node_modules/@uirouter/angularjs/release/angular-ui-router.js": {           
+            deps: "/node_modules/angular/angular.js"
+        }
+    },
+    packages:{
+        "controllers": {
+            defaultExtension:"js"
+        },
+        "directives":{
+            defaultExtension:"js"
+        },
+        ".":{
+            defaultExtension: "js"
         }
     }
 });
