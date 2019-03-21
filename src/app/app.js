3f3556e1d14344965b7eda34ce2ab3204fec8c49
Brian Juul Rasmussen <brian.juul.rasmussen@gmail.com>
2019-03-21 22:00:22 +0100

    removed js-files

--- src/app/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("bootstrap/dist/css/bootstrap.min.css"); // for at inkludere bootstrap i webpacks bundle...
-const app_module_1 = require("./app.module");
-app_module_1.AppModule.init();
-//# sourceMappingURL=app.js.map
