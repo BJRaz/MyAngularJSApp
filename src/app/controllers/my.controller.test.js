f308c1c94924e410134590e16829ca6274fd173c
Brian Juul Rasmussen <brian.juul.rasmussen@gmail.com>
2019-03-21 21:58:38 +0100

    removed js-files...

--- src/app/controllers/my.controller.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function testMe() {
-    console.log("Brian er smart");
-}
-exports.testMe = testMe;
-//# sourceMappingURL=my.controller.test.js.map
